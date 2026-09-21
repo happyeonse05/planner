5f1da23ef2672324d3a1a07dd2c91f2f8d1e6e70
happyeonse05 <yeonse051112@gmail.com>
2026-09-21 18:59:42 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase 연결 정보 (anon/publishable 키는 공개돼도 괜찮아요)
+window.PLANNER_CONFIG = {
+  supabaseUrl: 'https://mhxcnmfgemdnfjwmtqlh.supabase.co',
+  supabaseAnonKey: 'sb_publishable_PxgP4E_dgBowjQcv0093kw_xePXJh'
+};
